885b3fd0dc125266d92c228bbfa2086da63c7c34
georgina <georginachew@hotmail.sg>
2020-09-30 14:13:54 +0800

    aesthetical changes

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler, ConversationH
 from pytube import YouTube
 from moviepy.editor import *
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-
 def start(update, context):
     update.message.reply_text(
         'Hello, world! I will help you download Youtube videos in either .mp3 or .mp4 format. Simply use either /mp3 or /mp4 followed by the link to the video. Use /help for more.'
@@ -30,44 +24,11 @@ def mp3(update, context):
     fp2 = ('./downloads/' + yt.title + '.mp3')
     video = VideoFileClip(os.path.join(fp1))
     video.audio.write_audiofile(os.path.join(fp2))
-    # subprocess.call([
-    #     'ffmpeg',
-    #     '-i', os.path.join('./downloads', yt.title+'.mp4'), os.path.join('./downloads', yt.title+'.mp3')
-    # ], shell=True)
     vid = open(fp2, 'rb')
     update.message.reply_audio(vid)
     vid.close()
     os.remove(fp1)
     os.remove(fp2)
-
-    # if 'youtube' not in link:
-    #     update.message.reply_text('Please send a valid youtube link!')
-    # else:
-    #     l = urlopen(link)
-    #     contents = str(l.read())
-    #     print(type(contents))
-    #     # m = re.search('formats.+url.+mimeType.+;', contents)
-    #     # src = m.group(0)
-    #     # print(src)
-    #     src = contents[44550:-357000]
-    #     print('string slicer:', src)
-    #     m = re.search('formats.+:18.+url(.+)mimeType.+;', src)
-    #     src = m.group(0)
-    #     print(m.group(1))
-    #     print('src:', src)
-    #     # src = src[40:-15]
-    #     # print(src)
-    #     patt = '\\\\'
-    #     print(patt)
-    #     src = re.sub(patt, '', src)
-    #     print(src)
-    #     src = src[28:-10270]
-    #     print(src)
-    #     m2 = re.search('(https.+)","mimeType', src)
-    #     print(m2.group(0))
-    #     x = m2.group(0)
-    #     src = x[0:-11]
-    #     print(src)
 
 
 def mp4(update, context):
